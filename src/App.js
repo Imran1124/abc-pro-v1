fbd8bc7165ecd9bfd7dd18e80097be69ec093ffc
Imran1124 <gisimran01@outlook.com>
2022-02-15 16:56:58 +0530

    commit v2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import React, { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const NavBar = lazy(() => import('./Components/NavBar'))
+const Footer = lazy(() => import('./Components/Footer'))
+const LandingPage = lazy(() => import('./pages/LandingPage'))
+const ApplyJobCard = lazy(() => import('./pages/ApplyJobCard'))
+const ApplyJobCard1 = lazy(() => import('./pages/ApplyJobCard1'))
+const Contact = lazy(() => import('./pages/Contact'))
+const About = lazy(() => import('./pages/About'))
+const ApplyJobCard2 = lazy(() => import('./pages/ApplyJobCard2'))
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/applycard' element={<ApplyJobCard />} />
+        <Route path='/applycard1' element={<ApplyJobCard1 />} />
+        <Route path='/applycard2' element={<ApplyJobCard2 />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
